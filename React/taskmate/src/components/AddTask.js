c407ee17b3ea064e3b7317b82f624cdcd815ce44
shital1223 <shitalpatil94612@gmail.com>
2025-12-02 15:53:40 -0500

    Todo app with local storage.

---
+++ React/taskmate/src/components/AddTask.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+export const AddTask = ({ tasklist, setTasklist, task, setTask }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (task.id) {
+      const date = new Date();
+      const updatedTask = tasklist.map((todo) =>
+        todo.id === task.id
+          ? {
+              id: task.id,
+              name: task.name,
+              time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+            }
+          : todo
+      );
+      setTasklist(updatedTask);
+      setTask({});
+    } else {
+      const date = new Date();
+      const newTask = {
+        id: date.getTime(),
+        name: e.target.task.value,
+        time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+      };
+      setTasklist([...tasklist, newTask]);
+      setTask({});
+    }
+  };
+  return (
+    <section className="addTask">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={task.name || ""}
+          name="task"
+          autoComplete="off"
+          placeholder="Add Task"
+          onChange={(e) => setTask({ ...task, name: e.target.value })}
+        />
+        <button type="submit">{task.id ? "Update" : "Add"}</button>
+      </form>
+    </section>
+  );
+};
