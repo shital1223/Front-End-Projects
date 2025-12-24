e049483ad44908678568d233260de2525e7f62fd
shital1223 <shitalpatil94612@gmail.com>
2025-12-24 18:12:29 -0500

    Added Shopmate project

---
+++ React/shopmate/src/components/Header.js
@@ -0,0 +1,25 @@
+import { Link, NavLink } from "react-router-dom";
+import "./Header.css";
+import Logo from "../assets/logo.png";
+
+export const Header = () => {
+  return (
+    <header>
+      <Link to="/" className="logo">
+        <img src={Logo} alt="Shopmate Logo" />
+        <span>Shopmate</span>
+      </Link>
+      <nav className="navigation">
+        <NavLink to="/" className="link">
+          Home
+        </NavLink>
+        <NavLink to="/cart" className="link">
+          Cart
+        </NavLink>
+      </nav>
+      <Link to="/cart" className="items">
+        <span>Cart: 2</span>
+      </Link>
+    </header>
+  );
+};
